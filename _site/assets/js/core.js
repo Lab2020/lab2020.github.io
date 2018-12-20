05ad3ad86389148362a76b140010e1ecef36409d
kyawmyohtut1996 <kyawmyohtut@innovix-solutions.com>
2018-12-20 18:34:34 +0630

    banner change

--- _site/assets/js/core.js
+++ _site/assets/js/core.js
@@ -4,8 +4,16 @@ window.addEventListener('load', function() {
     let poly = document.querySelectorAll(".svg-slice polygon");
     let polyHeight = window.innerWidth/5;
     
-    for(var i=0; i<poly.length; i++) {
-        svg[i].setAttribute("height", polyHeight);
-        poly[i].setAttribute("points", `0,0 ${window.innerWidth},${polyHeight} 0,${polyHeight}`);
+    let setPolyHeight = () => {
+        for(var i=0; i<poly.length; i++) {
+            svg[i].setAttribute("height", polyHeight);
+            poly[i].setAttribute("points", `0,0 ${window.innerWidth},${polyHeight} 0,${polyHeight}`);
+        }
     }
+
+    setPolyHeight();
+
+    window.addEventListener('resize', function() {
+        setPolyHeight();
+    });
 });
