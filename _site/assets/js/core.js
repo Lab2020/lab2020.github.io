2dbfed18dd9f53ae0d94a206c867562b42d33155
lab2020 <kyawmyohtut.sead@outlook.com>
2018-12-23 17:22:31 +0630

    check at home

--- _site/assets/js/core.js
+++ _site/assets/js/core.js
@@ -4,16 +4,8 @@ window.addEventListener('load', function() {
     let poly = document.querySelectorAll(".svg-slice polygon");
     let polyHeight = window.innerWidth/5;
     
-    let setPolyHeight = () => {
-        for(var i=0; i<poly.length; i++) {
-            svg[i].setAttribute("height", polyHeight);
-            poly[i].setAttribute("points", `0,0 ${window.innerWidth},${polyHeight} 0,${polyHeight}`);
-        }
+    for(var i=0; i<poly.length; i++) {
+        svg[i].setAttribute("height", polyHeight);
+        poly[i].setAttribute("points", `0,0 ${window.innerWidth},${polyHeight} 0,${polyHeight}`);
     }
-
-    setPolyHeight();
-
-    window.addEventListener('resize', function() {
-        setPolyHeight();
-    });
 });
